06de96adcbdade93f575a14145fa5dc2f9568a81
fucknamel <1047760450@qq.com>
2019-02-25 17:13:49 +0800

    initial

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,2 +1,8 @@
-cats = require('./cats.js');
-console.log(cats)
+/*
+ * @Author: LKH 
+ * @Date: 2019-02-18 13:42:21 
+ * @Last Modified by: LKH
+ * @Last Modified time: 2019-02-18 15:01:19
+ */
+require('./index.css');
+$('body').html('NMSL');
---
+++ src/page/login/index.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: LKH 
+ * @Date: 2019-02-18 13:40:37 
+ * @Last Modified by: LKH
+ * @Last Modified time: 2019-02-18 13:43:46
+ */
+console.log('hello login');
