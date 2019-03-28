7172981c964023dee89d0c9c2f01a311402b4326
fucknamel <1047760450@qq.com>
2019-03-28 20:20:39 +0800

    index finished

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,14 +2,28 @@
  * @Author: LKH 
  * @Date: 2019-02-18 13:42:21 
  * @Last Modified by: LKH
- * @Last Modified time: 2019-03-11 12:56:03
+ * @Last Modified time: 2019-03-28 18:51:19
  */
 
+require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
+require('util/slider/index.js');
 var navSide = require('page/common/nav-side/index.js');
+var templateBanner = require('./banner.string');
 var _mm = require('util/mm.js');
 
-navSide.init({
-    name : 'about'
-});
+$(function(){
+    // 渲染banner的html
+    var bannerHtml = _mm.renderHtml(templateBanner);
+    $('.banner-con').html(bannerHtml);
+    // 初始化banner
+    var $slider = $('.banner').unslider({
+        dots : true
+    });
+    // 前一张和后一张操作的事件绑定
+    $('.banner-con .banner-arrow').click(function(){
+        var forward = $(this).hasClass('prev')?'prev':'next';
+        $slider.data('unslider')[forward]();
+    })
+})
---
+++ src/util/slider/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: LKH 
+ * @Date: 2019-03-26 18:39:32 
+ * @Last Modified by: LKH
+ * @Last Modified time: 2019-03-26 18:39:55
+ */
+
+require('./index.css');
+require('./unslider.js');
